dd10d1e1f3e364d2cba9f47bb48c9485470af78d
boa3444 <newone1804@gmail.com>
2025-03-27 02:23:34 +0530

    Add files via upload

---
+++ play Rock Paper Scissor With Computer.py
@@ -0,0 +1,72 @@
+rock = '''
+
+
+  _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+
+'''
+
+paper = '''
+    _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+
+
+'''
+
+scissor= '''
+   _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+
+'''
+print("PLAY rock , paper and scissors with the computer!!")
+
+#user's input
+user_input= input("Choose rock, paper or scissor!!!  ").lower()
+print(f"YOU CHOSE: {user_input} " )
+
+if user_input== 'rock':                         
+    print(rock)
+elif user_input== 'paper':
+    print(paper)
+elif user_input== 'scissor':
+    print(scissor)
+
+
+
+
+#computer plays random
+lis= [rock,paper,scissor]
+import random
+random_action =  random.choice(lis)
+
+print("COMPUTER CHOSE :"    + random_action )
+
+
+if (user_input== 'paper' and random_action == scissor) or    \
+   (user_input== 'scissor' and random_action == rock) or   \
+    (user_input=='rock' and random_action== paper):
+    print("YOU lose")
+
+elif(user_input== random_action):
+    print("Its a draw!!  Try again !!")
+
+else: 
+    print("You won!!  Congrats")
+
+
+
+
