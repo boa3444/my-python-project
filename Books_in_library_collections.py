067714d8c690d6f97576c4b5b07675ba1d9dbad2
Divyanshi thapa <newone1804@gmail.com>
2025-05-13 22:20:31 +0530

    Add files via upload

---
+++ Books_in_library_collections.py
@@ -0,0 +1,338 @@
+science_books = {
+    "A Brief History Of Time": {
+        "author": "Stephen Hawking",
+        "publishing_year": 1988
+    },
+    "Cosmos": {
+        "author": "Carl Sagan",
+        "publishing_year": 1980
+    },
+    "Silent Spring": {
+        "author": "Rachel Carson",
+        "publishing_year": 1962
+    },
+    "The Double Helix": {
+        "author": "James D. Watson",
+        "publishing_year": 1968
+    },
+    "Sapiens: A Brief History Of Humankind":{
+        "author": "Yuval Noah Harari",
+        "publishing_year": 2011
+    },
+    "The Origin Of Species": {
+        "author": "Charles Darwin",
+        "publishing_year": 1859
+    },
+    "Newton's Principia": {
+        "author": "Isaac Newton",
+        "publishing_year": 1687
+    },
+    "The Selfish Gene": {
+        "author": "Richard Dawkins",
+        "publishing_year": 1976
+    },
+    "Genome: The Autobiography Of A Species In 23 Chapters": {
+        "author": "Matt Ridley",
+        "publishing_year": 1999
+    },
+    "Six Easy Pieces: Essentials Of Physics Explained By Its Most Brilliant Teacher": {
+        "author": "Richard Feynman",
+        "publishing_year": 1995
+    }
+}
+fantasy_books = {
+    "The Lord Of The Rings": {
+        "publishing_year": 1954,
+        "author": "J.R.R. Tolkien",
+        
+    },
+    "A Game Of Thrones": {
+        "publishing_year": 1996,
+        "author": "George R.R. Martin"
+    },
+    "Harry Potter And The Sorcerer's Stone": {
+        "publishing_year": 1997,
+        "author": "J.K. Rowling"
+    },
+    "The Name Of The Wind": {
+        "publishing_year": 2007,
+        "author": "Patrick Rothfuss"
+    },
+    "Mistborn: The Final Empire": {
+        "publishing_year": 2006,
+        "author": "Brandon Sanderson"
+    },
+    "Jonathan Strange & Mr Norrell":{
+        "publishing_year": 2004,
+        "author": "Susanna Clarke"
+    },
+    "The Hobbit": {
+        "publishing_year": 1937,
+        "author": "J.R.R. Tolkien"
+    },
+    "The Lion, the Witch, And The Wardrobe": {
+        "publishing_year": 1950,
+        "author": "C.S. Lewis"
+    },
+    "The Colour Of Magic": {
+        "publishing_year": 1983,
+        "author": "Terry Pratchett"
+    },
+    "Guards! Guards!": {
+        "publishing_year": 1989,
+        "author": "Terry Pratchett"
+    }
+
+}
+encyclopedias = {
+    "Encyclopædia Britannica": {
+        "publisher": "Encyclopædia Britannica, Inc.",
+        "first_published": 1768
+    },
+    "Encyclopedia Americana": {
+        "publisher": "Scholastic Library Publishing",
+        "first_published": 1820
+    },
+    "Brockhaus Enzyklopädie": {
+        "publisher": "F.A. Brockhaus",
+        "first_published": 1808
+    },
+    "Gran Enciclopedia Argentina":{
+        "publisher": "Omeba",
+        "first_published": 1956
+    },
+    "Great Soviet Encyclopedia":{
+        "publisher": "State Scientific Institution",
+        "first_published": 1926
+    },
+    "Larousse Encyclopedia": {
+        "publisher": "Éditions Larousse",
+        "first_published": 1865
+    },
+    "World Book Encyclopedia": {
+        "publisher": "World Book, Inc.",
+        "first_published": 1917
+    },
+    "The Canadian Encyclopedia": {
+        "publisher": "Historica Canada",
+        "first_published": 1985
+    },
+    "Svensk Uppslagsbok":{
+        "publisher": "Förlagshuset Norden",
+        "first_published": 1929
+    },
+    "Aschehougs Konversasjonsleksikon":{
+        "publisher": "Aschehoug",
+        "first_published": 1907
+    }
+}
+horror_books = {
+    "Dracula": {
+        "author": "Bram Stoker",
+        "publishing_year": 1897
+    },
+    "Frankenstein": {
+        "author": "Mary Shelley",
+        "publishing_year": 1818
+    },
+    "The Shining": {
+        "author": "Stephen King",
+        "publishing_year": 1977
+    },
+    "IT": {
+        "author": "Stephen King",
+        "publishing_year": 1986
+    },
+    "The Haunting Of Hill House": {
+        "author": "Shirley Jackson",
+        "publishing_year": 1959
+    },
+    "The Exorcist": {
+        "author": "William Peter Blatty",
+        "publishing_year": 1971
+    },
+    "The Picture Of Dorian Gray": {
+        "author": "Oscar Wilde",
+        "publishing_year": 1890
+    },
+    "Interview With The Vampire": {
+        "author": "Anne Rice",
+        "publishing_year": 1976
+    },
+    "Pet Sematary":{
+        "author": "Stephen King",
+        "publishing_year": 1983
+    },
+    "Bird Box":{
+        "author": "Josh Malerman",
+        "publishing_year": 2014
+    }
+}
+
+money_books = {
+    "Rich Dad Poor Dad": {
+        "author": "Robert Kiyosaki",
+        "publishing_year": 1997
+    },
+    "The Intelligent Investor": {
+        "author": "Benjamin Graham",
+        "publishing_year": 1949
+    },
+    "Think and Grow Rich": {
+        "author": "Napoleon Hill",
+        "publishing_year": 1937
+    },
+    "The Total Money Makeover": {
+        "author": "Dave Ramsey",
+        "publishing_year": 2003
+    },
+    "Your Money or Your Life": {
+        "author": "Vicki Robin and Joe Dominguez",
+        "publishing_year": 1992
+    },
+    "The Richest Man in Babylon": {
+        "author": "George S. Clason",
+        "publishing_year": 1926
+    },
+    "Money Master the Game": {
+        "author": "Tony Robbins",
+        "publishing_year": 2014
+    },
+    "I Will Teach You to Be Rich": {
+        "author": "Ramit Sethi",
+        "publishing_year": 2009
+    },
+     "The Psychology of Money":{
+        "author": "Morgan Housel",
+        "publishing_year": 2020
+    },
+    "A Random Walk Down Wall Street":{
+        "author": "Burton Malkiel",
+        "publishing_year": 1973
+    },
+    "The Millionaire Next Door": {
+        "author": "Thomas J. Stanley and William D. Danko",
+        "publishing_year": 1996
+    },
+    "The Automatic Millionaire": {
+        "author": "David Bach",
+        "publishing_year": 2003
+    },
+    "The Little Book of Common Sense Investing": {
+        "author": "John C. Bogle",
+        "publishing_year": 2007
+    },
+    "Secrets of the Millionaire Mind": {
+        "author": "T. Harv Eker",
+        "publishing_year": 2005
+    },
+    "The Warren Buffett Way": {
+        "author": "Robert Hagstrom",
+        "publishing_year": 1994
+    }
+}
+
+self_help_books = {
+    "How to Win Friends and Influence People": {
+        "author": "Dale Carnegie",
+        "publishing_year": 1936
+    },
+    "The 7 Habits of Highly Effective People": {
+        "author": "Stephen R. Covey",
+        "publishing_year": 1989
+    },
+    "Daring Greatly": {
+        "author": "Brené Brown",
+        "publishing_year": 2012
+    },
+    "Mindset: The New Psychology of Success": {
+        "author": "Carol S. Dweck",
+        "publishing_year": 2006
+    },
+    "The Power of Now": {
+        "author": "Eckhart Tolle",
+        "publishing_year": 1997
+    },
+    "The Subtle Art of Not Giving a F*ck": {
+        "author": "Mark Manson",
+        "publishing_year": 2016
+    },
+    "Thinking, Fast and Slow": {
+        "author": "Daniel Kahneman",
+        "publishing_year": 2011
+    },
+    "Atomic Habits": {
+        "author": "James Clear",
+        "publishing_year": 2018
+    },
+    "The 4-Hour Workweek": {
+        "author": "Timothy Ferriss",
+        "publishing_year": 2007
+    },
+    "Man's Search for Meaning": {
+        "author": "Viktor E. Frankl",
+        "publishing_year": 1946
+    },
+    "Attached":{
+        "author": "Amir Levine and Rachel Heller",
+        "publishing_year": 2010
+    },
+    "The Gifts of Imperfection":{
+        "author": "Brené Brown",
+        "publishing_year": 2010
+    },
+    "12 Rules for Life":{
+        "author": "Jordan Peterson",
+        "publishing_year": 2018
+    },
+    "Feeling Good: The New Mood Therapy":{
+        "author": "David D. Burns",
+        "publishing_year": 1980
+    },
+    "Slight Edge":{
+        "author": "Jeff Olson",
+        "publishing_year": 2005
+    }
+}
+fiction_books = {
+    "To Kill a Mockingbird": {
+        "author": "Harper Lee",
+        "publishing_year": 1960
+    },
+    "Pride and Prejudice": {
+        "author": "Jane Austen",
+        "publishing_year": 1813
+    },
+    "The Lord of the Rings": {
+        "author": "J.R.R. Tolkien",
+        "publishing_year": 1954
+    },
+    "1984": {
+        "author": "George Orwell",
+        "publishing_year": 1949
+    },
+    "The Great Gatsby": {
+        "author": "F. Scott Fitzgerald",
+        "publishing_year": 1925
+    },
+    "Jane Eyre":{
+        "author":"Charlotte Brontë",
+        "publishing_year":1847
+    },
+    "Little Women":{
+        "author":"Louisa May Alcott",
+        "publishing_year": 1868
+    },
+    "The Catcher in the Rye":{
+        "author":"J. D. Salinger",
+        "publishing_year":1951
+    },
+    "Animal Farm":{
+        "author":"George Orwell",
+        "publishing_year":1945
+    },
+    "The Hobbit":{
+        "author":"J.R.R. Tolkien",
+        "publishing_year":1937
+    }
+}
