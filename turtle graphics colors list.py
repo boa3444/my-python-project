ee8c3d7ae765745077b5a638775761b7a9bf8a6b
Divyanshi thapa <newone1804@gmail.com>
2025-06-04 12:34:31 +0530

    Add files via upload

---
+++ turtle graphics colors list.py
@@ -0,0 +1,23 @@
+turtle_colors = [
+    'snow', 'ghost white', 'white smoke', 'gainsboro', 'floral white', 'old lace',
+    'linen', 'antique white', 'papaya whip', 'blanched almond', 'bisque', 'peach puff',
+    'navajo white', 'lemon chiffon', 'mint cream', 'azure', 'alice blue', 'lavender',
+    'lavender blush', 'misty rose', 'white', 'black', 'dark slate gray', 'dim gray',
+    'slate gray', 'light slate gray', 'gray', 'light grey', 'midnight blue', 'navy',
+    'cornflower blue', 'dark slate blue', 'slate blue', 'medium slate blue', 'light slate blue',
+    'medium blue', 'royal blue', 'blue', 'dodger blue', 'deep sky blue', 'sky blue',
+    'light sky blue', 'steel blue', 'light steel blue', 'light blue', 'powder blue',
+    'pale turquoise', 'dark turquoise', 'medium turquoise', 'turquoise', 'cyan', 'light cyan',
+    'cadet blue', 'medium aquamarine', 'aquamarine', 'dark green', 'dark olive green',
+    'dark sea green', 'sea green', 'medium sea green', 'light sea green', 'pale green',
+    'spring green', 'lawn green', 'medium spring green', 'green yellow', 'lime green',
+    'yellow green', 'forest green', 'olive drab', 'dark khaki', 'khaki', 'pale goldenrod',
+    'light goldenrod yellow', 'light yellow', 'yellow', 'gold', 'light goldenrod',
+    'goldenrod', 'dark goldenrod', 'rosy brown', 'indian red', 'saddle brown', 'sienna',
+    'peru', 'burlywood', 'beige', 'wheat', 'sandy brown', 'tan', 'chocolate', 'firebrick',
+    'brown', 'dark salmon', 'salmon', 'light salmon', 'orange', 'dark orange', 'coral',
+    'light coral', 'tomato', 'orange red', 'red', 'hot pink', 'deep pink', 'pink',
+    'light pink', 'pale violet red', 'maroon', 'medium violet red', 'violet red',
+    'medium orchid', 'dark orchid', 'dark violet', 'blue violet', 'purple', 'medium purple',
+    'thistle'
+]
