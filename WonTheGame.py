e190797ed6404f911498642ca34e195384f072a9
Divyanshi thapa <newone1804@gmail.com>
2025-06-25 11:56:31 +0530

    Add files via upload

---
+++ WonTheGame.py
@@ -0,0 +1,11 @@
+from turtle import Turtle
+
+
+def you_won():
+    you_won = Turtle()
+    you_won.color("yellow")
+    you_won.hideturtle()
+    you_won.write(arg= "YOU WON\nTHE GAME\nCONGRATS", align=  "center", font = ("Times New Roman", 50, "normal"))
+
+
+
